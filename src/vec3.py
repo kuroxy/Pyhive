6e8145826b07246c0762a3930eca1b43701d94ed
Okke Roelofsen <okkeroelofsen@gmail.com>
2023-11-24 00:02:11 +0100

    adding a helperclass

---
+++ src/vec3.py
@@ -0,0 +1,44 @@
+
+class Vec3:
+    neighbours = [[0, 1, 0], [1, 0, 0], [1, -1, 0], [0, -1, 0],
+                  [-1, 0, 0], [-1, 1, 0], [0, 0, 1], [0, 0, -1]]
+
+    def __init__(self, plist: list) -> None:
+        self._poslist = [0, 0, 0]
+        if len(plist) == 3:
+            self._poslist = plist
+
+    def __eq__(self, other):
+        if isinstance(other, Vec3):
+            return self._poslist == other._poslist
+        return False
+
+    def get_neighbours(self):
+        neighs = []
+        for pos in Vec3.neighbours:
+            neighs.append(Vec3([pos[0]+self.x, pos[1]+self.y, pos[2]+self.z]))
+        return neighs
+
+    @property
+    def x(self):
+        return self._poslist[0]
+
+    @x.setter
+    def x(self, value: int):
+        self._poslist[0] = value
+
+    @property
+    def y(self):
+        return self._poslist[1]
+
+    @y.setter
+    def y(self, value: int):
+        self._poslist[1] = value
+
+    @property
+    def z(self):
+        return self._poslist[2]
+
+    @z.setter
+    def z(self, value: int):
+        self._poslist[2] = value
---
+++ tests/vec3_test.py
@@ -0,0 +1,44 @@
+from src.vec3 import Vec3
+import unittest
+
+
+class TestNodeMethods(unittest.TestCase):
+    def test_init_getters(self):
+        v = Vec3([1, 2, 3])
+
+        self.assertEqual(v._poslist, [1, 2, 3])
+
+        self.assertEqual(v.x, 1)
+        self.assertEqual(v.y, 2)
+        self.assertEqual(v.z, 3)
+
+    def test_equal(self):
+        v = Vec3([1, 2, 3])
+        v2 = Vec3([1, 2, 3])
+
+        self.assertEqual(v, v)
+        self.assertEqual(v, v2)
+
+    def test_unequal(self):
+        v = Vec3([1, 2, 3])
+        v2 = Vec3([1, 2, 3])
+        self.assertNotEqual(v, v2)
+
+    def test_get_neighs(self):
+        v = Vec3([0, 0, 0])
+        neighs = [[0, 1, 0], [1, 0, 0], [1, -1, 0], [0, -1, 0],
+                  [-1, 0, 0], [-1, 1, 0], [0, 0, 1], [0, 0, -1]]
+        neighsvec3 = [Vec3(l) for l in neighs]
+        self.assertEqual(v.get_neighbours(), neighsvec3)
+
+    def test_get_neighs2(self):
+        v = Vec3([1, 2, 3])
+        neighs = [[1, 3, 3], [2, 2, 3], [2, 1, 3], [1, 1, 3],
+                  [0, 2, 3], [0, 3, 3], [1, 2, 4], [1, 2, 2]]
+        neighsvec3 = [Vec3(l) for l in neighs]
+
+        self.assertEqual(v.get_neighbours(), neighsvec3)
+
+
+if __name__ == '__main__':
+    unittest.main()
